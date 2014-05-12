f45bfe7885282504dbf27338160cde0115327d81
Peter Schwarz <peter.schwarz@bicycle.io>
2014-05-12 12:57:44 -0500

    Added optional args for open-port and open-channel-port; testing stubs

--- src/main/clojure/gpio/core.clj
+++ src/main/clojure/gpio/core.clj
@@ -43,17 +43,12 @@
   (set-direction! [port direction] "Sets the direction of this port: in or out.")
   (set-active-low! [port active-low?] "Invert the logic of the value pin for both reading and writing so that a high == 0 and low == 1. ")
   (read-value [port] "Return the value of the port")
-  (write-value! [port value] "Writes the value of the port"))
+  (write-value! [port value] "Writes the value to the port.  The value may be specified as :high, :low (and symbol or string variations), \1, \0, or 1, 0"))
 
 (defprotocol GpioChannelProvider
   (set-edge! [providor setting])
   (create-edge-channel [port] "Returns a core async channel on which events will be put")
   (release-edge-channel! [port channel]))
-
-(defn- read-high-low [raf]
-  (.seek raf 0)
-  ; TODO: Add this as a configurable conversion function
-  (if (= \1 (char (.read raf))) :high :low))
 
 (defn- value-file [port]
   (str "/sys/class/gpio/gpio" port "/value"))
@@ -66,47 +61,66 @@
   Args:
   * port - the gpio pin number
   * opts: a map of optional properts
-      * :from-raw-fn: a converter function, which takes the `char`
+      * :direction - sets the initial direction
+      * :active-low? - sets the port as \"active low\", value true or false
+      * :initial-value - sets the intitial value of the pin.
+      * :from-raw-fn - a converter function, which takes the `char`
           value (\1 or \0) read and converts it into a meaningful value.
           The default converts the values to :high and :low respectively "
-  [port & opts]
+  [port & [opts]]
   (export! port)
-  (let [{:keys [from-raw-fn]
+  (let [{:keys [from-raw-fn direction active-low? initial-value]
          :or {from-raw-fn #(if (= \1 %) :high :low)}} opts
         filename (value-file port)
         raf (random-access filename)
-        props {:port port, :file-name filename, :file raf}]
-    (reify
-      clojure.lang.ILookup
+        props {:port port, :file-name filename, :file raf}
+        gpio-port (reify
+                   clojure.lang.ILookup
 
-      (valAt [_ k not-found] (get props k not-found))
-      (valAt [_ k] (k props))
+                   (valAt [_ k not-found] (get props k not-found))
+                   (valAt [_ k] (k props))
 
-      GpioPort
+                   GpioPort
 
-      (set-direction! [_ direction] (do-set-direction! port direction))
+                   (set-direction! [_ direction] (do-set-direction! port direction))
 
-      (set-active-low! [_ active-low?] (do-set-active-low port active-low?))
+                   (set-active-low! [_ active-low?] (do-set-active-low port active-low?))
 
-      (read-value
-       [_]
-       (.seek raf 0)
-       (from-raw-fn (char (.read raf))))
+                   (read-value
+                    [_]
+                    (.seek raf 0)
+                    (from-raw-fn (char (.read raf))))
 
-      (write-value!
-       [_ value]
-       (.seek raf 0)
-       (.writeByte raf (high-low-value value)))
+                   (write-value!
+                    [_ value]
+                    (.seek raf 0)
+                    (.writeByte raf (high-low-value value)))
 
-      Closeable
-      (close! [_]
-              (.close raf)
-              (unexport! port)))))
+                   Closeable
+                   (close! [_]
+                           (.close raf)
+                           (unexport! port)))]
+    (try
+      (when direction (set-direction! gpio-port direction))
+      (when active-low? (set-active-low! gpio-port active-low?))
+      (when initial-value (write-value! gpio-port initial-value))
+      gpio-port
+      (catch Exception e
+        (close! gpio-port)
+        (throw e)))
+
+))
 
 (defn open-channel-port
-  "Opens a port which can be used for listening to events which occur on the 0"
-  [port & opts]
-  (let [create-channel (fn [] (chan (sliding-buffer (:event-buffer-size opts))))
+  "Opens a port which can be used for listening to value changes.  In addition to the
+  options map for a basic port, the following are available:
+  * :event-buffer-size - the size of the change events buffer.  Defaults to 1
+  * :timeout - sets a timeout for the OS polling wait time.  Defaults to -1, which waits indefinitely
+  * :edge - sets the edge value- :rising, :falling, or :both"
+  [port & [opts]]
+  (let [{:keys [event-buffer-size timeout edge]
+         :or {event-buffer-size 1, timeout -1}} opts
+        create-channel (fn [] (chan (sliding-buffer event-buffer-size)))
         gpio-port (open-port port opts)
         poller (EventPolling/create)
         write-ch (chan 1)
@@ -114,6 +128,8 @@
         mult-ch (a/mult read-ch)]
 
     (.addFile poller (:file gpio-port) (bit-or EventPolling/EPOLLIN EventPolling/EPOLLET EventPolling/EPOLLPRI) gpio-port)
+
+    (when edge (do-set-edge! port edge))
 
     ; Serialize the write loop
     (go (loop []
@@ -123,7 +139,7 @@
 
     (go (loop []
           ; TODO: Let's use a timeout for more predictable shutdowns
-          (when-let [events (.poll poller -1)]
+          (when-let [events (.poll poller timeout)]
             (doseq [_ (filter #(=  gpio-port (.getData %)) events)]
               (>! read-ch (read-value gpio-port)))
             (recur))))
---
+++ src/test/clojure/gpio_core_test.clj
@@ -0,0 +1,9 @@
+(ns gpio-core-test
+  (:require [clojure.test :refer :all]
+            [gpio.core :refer :all]))
+
+(deftest test-open-port
+
+  )
+
+(run-tests)
