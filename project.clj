ed784c1ff3083b8db62721b6127233b46320ccc7
Peter Schwarz <peter.schwarz@bicycle.io>
2015-01-28 11:11:46 -0600

    update deps

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-gpio "0.0.1-SNAPSHOT"
+(defproject clj-gpio "0.1.0-SNAPSHOT"
     :description "A lightweight Clojure library for Raspberry PI GPIO"
     :url "https://github.com/peterschwarz/clj-gpio"
     :min-lein-version  "2.0.0"
@@ -7,6 +7,6 @@
     :java-source-paths ["src/main/java"]
     :javac-options     ["-target" "1.6" "-source" "1.6"]
 
-    :dependencies [[org.clojure/clojure "1.5.1"]
-                   [org.clojure/core.async "0.1.278.0-76b25b-alpha"]
+    :dependencies [[org.clojure/clojure "1.6.0"]
+                   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                    [net.java.dev.jna/jna "4.1.0"]])
