2b63cf46ca0e6f51a8210329fd236dc1971457bc
Peter Schwarz <peter.schwarz@bicycle.io>
2015-12-28 15:42:19 -0600

    updated deps

--- project.clj
+++ project.clj
@@ -9,7 +9,7 @@
     :java-source-paths ["src/main/java"]
     :javac-options     ["-target" "1.6" "-source" "1.6"]
 
-    :dependencies [[org.clojure/clojure "1.6.0"]
-                   [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-                   [net.java.dev.jna/jna "4.1.0"]])
+    :dependencies [[org.clojure/clojure "1.7.0"]
+                   [org.clojure/core.async "0.2.374"]
+                   [net.java.dev.jna/jna "4.2.1"]])
 
